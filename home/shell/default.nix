d4811f9a6c5dce90de9e6ee7c6627194c708528a
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-18 16:06:23 +1000

    bashrc done for now

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -8,5 +8,19 @@
   };
 
   programs = {
+    bash = {
+      enable = true;
+      enableCompletion = true;
+
+
+      # Custom prompt settings
+      initExtra = ''
+        PS1='[\u@\h \W]\$ '
+        PROMPT_DIRTRIM=1
+        PS1="\[\e[0;35m\]\u@\h \[\e[0;95m\]\w \[\e[0;32m\]\$ \[\e[0;m\]"
+      '';
+      
+        # Some more bashrc shit goes here
+    };
   };
 }
