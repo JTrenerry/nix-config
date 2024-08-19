48bc52ace9ba6048bda914c38e766a20551f48bc
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-19 16:04:49 +1000

    adds git

---
+++ home/modules/tui/git.nix
@@ -0,0 +1,16 @@
+{
+  programs = {
+    git = {
+      enable = true;
+
+      userEmail = "105094182+JTrenerry@users.noreply.github.com";
+      userName = "JTrenerry";
+
+      extraConfig = {
+        core.editor = "nvim";
+        github.user = "JTrenerry";
+        pull.rebase = false;
+      };
+    };
+  };
+}
