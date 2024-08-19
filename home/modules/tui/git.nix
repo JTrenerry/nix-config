7aea7d7972bd3aee0891065728deaa942c45af3d
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-19 16:14:16 +1000

    55 current  2024-08-19 16:14:12  24.11.20240814.c3aa7b8   6.6.45                          *

--- home/modules/tui/git.nix
+++ home/modules/tui/git.nix
@@ -7,6 +7,10 @@
       userName = "JTrenerry";
 
       extraConfig = {
+        url."git@github.com:".insteadOf = "https://github.com/";
+        init.defaultBranch = "main";
+        core.autocrlf = "input";
+        push.default = "current";
         core.editor = "nvim";
         github.user = "JTrenerry";
         pull.rebase = false;
