cc548e9374616fd21a6670d289d5266244d14ce0
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-20 17:03:14 +1000

    75 current  2024-08-20 17:03:04  24.11.20240814.c3aa7b8   6.6.45                          *

--- home/modules/tui/neofetch.nix
+++ home/modules/tui/neofetch.nix
@@ -3,30 +3,31 @@
 {
   home.file.".config/neofetch/config.conf".text = ''
     print_info() {
-    info title
-    info underline
+      info title
+      info underline
 
-    info "OS" distro
-    info "Host" model
-    info "Kernel" kernel
-    info "Uptime" uptime
-    info "Packages" packages
-    info "Shell" shell
-    info "Resolution" resolution
-    info "DE" de
-    info "WM" wm
-    info "WM Theme" wm_theme
-    info "Theme" theme
-    info "Icons" icons
-    info "Terminal" term
-    info "Terminal Font" term_font
-    info "CPU" cpu
-    info "GPU" gpu
-    info "Memory" memory
-    info "Battery" battery
-    info "Font" font
-    info "Song" song
+      info "OS" distro
+      info "Host" model
+      info "Kernel" kernel
+      info "Uptime" uptime
+      info "Packages" packages
+      info "Shell" shell
+      info "Resolution" resolution
+      info "DE" de
+      info "WM" wm
+      info "WM Theme" wm_theme
+      info "Theme" theme
+      info "Icons" icons
+      info "Terminal" term
+      info "Terminal Font" term_font
+      info "CPU" cpu
+      info "GPU" gpu
+      info "Memory" memory
+      info "Battery" battery
+      info "Font" font
+      info "Song" song
 
-    info cols
+      info cols
+    }
   '';
 }
