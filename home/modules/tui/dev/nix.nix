18cf78767d18bb83159b62b645b944a217994297
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-20 16:24:41 +1000

    73 current  2024-08-20 16:24:37  24.11.20240814.c3aa7b8   6.6.45                          *

--- home/modules/tui/dev/nix.nix
+++ home/modules/tui/dev/nix.nix
@@ -1,9 +1,12 @@
 {pkgs, ...}: {
   programs = {
     nixvim = {
+      extraPackages = with pkgs; [
+      ];
       plugins = {
         lsp.servers.nil-ls.enable = true;
         lsp-format.lspServersToEnable = ["nil-ls"];
+        nix.enable = true;
       };
     };
   };
