5bb09cfe74669e1e5a250f44c9016da2d0924beb
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-21 15:17:46 +1000

    124 current  2024-08-21 15:17:40  24.11.20240814.c3aa7b8   6.6.45                          *

--- home/modules/gui/hyprland.nix
+++ home/modules/gui/hyprland.nix
@@ -178,6 +178,9 @@
         ", PRINT, exec, hyprshot -m region"
 
         # Audio
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
 
         # Brightness
         ",XF86MonBrightnessDown,exec,brightnessctl set 5%-"
