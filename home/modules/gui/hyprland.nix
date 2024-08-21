c26b4b38ab6862c594d52be7d52a8eb5488ab066
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-21 15:04:37 +1000

    122 current  2024-08-21 15:04:31  24.11.20240814.c3aa7b8   6.6.45                          *

--- home/modules/gui/hyprland.nix
+++ home/modules/gui/hyprland.nix
@@ -180,8 +180,8 @@
         # Audio
 
         # Brightness
-        "bind=,XF86MonBrightnessDown,exec,brightnessctl set 5%-"
-        "bind=,XF86MonBrightnessUp,exec,brightnessctl set +5%"
+        ",XF86MonBrightnessDown,exec,brightnessctl set 5%-"
+        ",XF86MonBrightnessUp,exec,brightnessctl set +5%"
       ];
 
       bindm = [
