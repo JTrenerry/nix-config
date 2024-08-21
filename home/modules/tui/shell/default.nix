c22ff8e40d554d890ebd0e9b468490fcd6b64ea6
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-21 12:52:32 +1000

    112 current  2024-08-21 12:52:26  24.11.20240814.c3aa7b8   6.6.45                          *

--- home/modules/tui/shell/default.nix
+++ home/modules/tui/shell/default.nix
@@ -10,29 +10,10 @@
   programs = {
     bash = {
       enable = true;
-      enableCompletion = true;
+    };
 
-
-      # Custom prompt settings
-      initExtra = ''
-        GREEN="\[\e[32m\]"
-        BLUE="\[\e[34m\]"
-        PURPLE="\[\e[35m\]"
-        YELLOW="\[\e[33m\]"
-        RED="\[\e[31m\]"
-        RESET="\[\e[0m\]"
-
-        # Function to get the current git branch
-        function parse_git_branch() {
-          git branch 2>/dev/null | grep '*' | sed 's/* //'
-        }
-
-        # Create a colorful prompt with git branch info and symbols
-        export PS1="\[\e[35m\]\u \w \$(parse_git_branch) $ \[\e[0m\]"
-
-        # Make sure to not use libgit2
-        unset LIBGIT2_NO_VENDOR
-      '';
+    starship = {
+      enable = true;
     };
   };
 }
