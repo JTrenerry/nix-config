2c2273a63beb1c3e20f513d5fe84ffa82aa9bfc0
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-22 17:02:32 +1000

    129 current  2024-08-22 17:02:27  24.11.20240814.c3aa7b8   6.6.45                          *

--- nixos/hosts/glaceon/scripts/move.nix
+++ nixos/hosts/glaceon/scripts/move.nix
@@ -2,5 +2,5 @@
 
 pkgs.writeShellScriptBin "move" ''
     class = ''${1^^}
-    cd "''${HOME}/Documents/University/${class:0:4}/${class:4:4}" || exit
+    cd "''${HOME}/Documents/University/''${class:0:4}/''${class:4:4}" || exit
 ''
