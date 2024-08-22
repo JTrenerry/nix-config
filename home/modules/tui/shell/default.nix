7c600642b5e5fe1974b9cd860d222f22e31e3bec
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-23 09:26:32 +1000

    138 current  2024-08-23 09:26:23  24.11.20240814.c3aa7b8   6.6.45                          *

--- home/modules/tui/shell/default.nix
+++ home/modules/tui/shell/default.nix
@@ -22,6 +22,35 @@
 
     starship = {
       enable = true;
+      settings = {
+        battery = {
+          full_symbol = "🔋";
+          charging_symbol = "⚡️";
+          discharging_symbol = "🔌";
+          disabled = false;
+        };
+
+        git_branch = {
+          symbol = "🌱 "; # You can customize the symbol here
+          disabled = false;
+        };
+
+        # Add other configurations as needed
+        prompt_order = [
+          "username"
+          "hostname"
+          "directory"
+          "git_branch"
+          "battery"
+          "jobs"
+          "time"
+        ];
+
+        # Example time format
+        time = {
+          format = "🕒 [%H:%M:%S]";
+        };
+      };
     };
   };
 }
