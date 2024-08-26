6e0da26122e9dadd75b7c8704dfedce0e3b2f704
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-26 13:37:50 +1000

    158 current  2024-08-26 13:37:42  24.11.20240821.c374d94   6.6.47                          *

--- nixos/hosts/glaceon/configuration.nix
+++ nixos/hosts/glaceon/configuration.nix
@@ -20,9 +20,6 @@ in
     ];
 
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
