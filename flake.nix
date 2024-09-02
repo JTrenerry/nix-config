c07f5852421867a4e3f586cdc46dfb405ab55d46
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-09-02 11:17:48 +1000

    219 current  2024-08-31 08:45:49  24.11.20240824.d0e1602  6.6.47                          *

--- flake.nix
+++ flake.nix
@@ -20,12 +20,24 @@
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
+
+    # 3200 overlay
+    lwjgl-overlay = {
+      url = "github:plapadoo/gdx-nixos-overlay"; # Replace with the actual username/repo
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, lwjgl-overlay, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
+      system = "x86_64-linux";
       modules = [
+        ({ pkgs, ...}: {
+          nixpkgs.overlays = [
+            (import (lwjgl-overlay + "/default.nix"))
+          ];
+        })
         ./nixos/hosts/glaceon/configuration.nix
         inputs.home-manager.nixosModules.default
       ];
