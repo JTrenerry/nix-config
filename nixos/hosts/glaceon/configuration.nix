85099d18e841f23c370e5df6556b19651005a532
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-09-03 18:17:37 +1000

    g

--- nixos/hosts/glaceon/configuration.nix
+++ nixos/hosts/glaceon/configuration.nix
@@ -173,8 +173,8 @@ in
   stylix = {
     enable = true;
     autoEnable = true;
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
-    homeManagerIntegration.autoImport = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
+    #homeManagerIntegration.autoImport = true;
   };
 
   # Some programs need SUID wrappers, can be configured further or are
