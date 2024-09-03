810b443f8d985e581bebcf4375b2ba49b78e1723
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-09-04 09:51:24 +1000

    241 current  2024-09-04 09:51:16  24.11.20240824.d0e1602  6.6.47                          *

--- home/modules/gui/stylix.nix
+++ home/modules/gui/stylix.nix
@@ -9,10 +9,15 @@
   stylix = {
     enable = true;
     autoEnable = true;
+    polarity = "dark";
     targets = {
       hyprland.enable = false;
       firefox.enable = false;
       rofi.enable = false;
+      dunst.enable = false;
+      kitty.enable = false;
+      nixvim.enable = false;
+      waybar.enable = false;
     };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
     image = ../gui/hyprland/wallpaper.jpg;
